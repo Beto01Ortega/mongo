f9615e4fcb3a414d7aad38f9c853b97a7727785d
Beto Ortega <betoandriu01@gmail.com>
2020-03-22 17:20:53 -0500

    Actualizar usuario

--- routes/user.js
+++ routes/user.js
@@ -4,9 +4,11 @@ var express = require('express');
 var UserController = require('../controllers/user');
 
 var api = express.Router();
+var md_auth = require('../middlewares/authenticated');
 
-api.get('/probandoControlador', UserController.pruebas);
+api.get('/probandoControlador',md_auth.ensureAuth , UserController.pruebas);
 api.post('/register', UserController.saveUser);
 api.post('/login', UserController.loginUser);
+api.put('/update-user/:id',md_auth.ensureAuth, UserController.updateUser);
 
 module.exports = api;
